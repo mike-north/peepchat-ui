8f304c61ae3e48e0df5f7e7ddb13273a38cbde23
Mike North <michael.l.north@gmail.com>
2016-04-30 20:53:31 +0800

    Rooms implementation
    Signed-off-by: Mike North <michael.l.north@gmail.com>

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,10 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   email: attr('string'),
   password: attr('string'),
-  passwordConfirmation: attr('string')
+  passwordConfirmation: attr('string'),
+  rooms: hasMany('room')
 });
--- app/routes/app/index.js
+++ app/routes/app/index.js
@@ -1,4 +1,53 @@
 import Ember from 'ember';
 
+const { RSVP, inject } = Ember;
+
 export default Ember.Route.extend({
+
+  flashMessages: inject.service(),
+
+  actions: {
+    /* Create a new room */
+    createRoom() {
+      // Get the "new room" model
+      let data = this.get('currentModel.newRoom');
+      // Create an ember-data record
+      let room = this.store.createRecord('room', {name: data.name});
+      // Clear any existing error messages
+      this.set('currentModel.newRoom.errors', []);
+
+      room.save().then(() => { // Successful creation
+        // Notification of success!
+        this.get('flashMessages').success(`Created room: ${data.name}`);
+        this.set('currentModel.newRoom.name', ''); // Clear the input
+      }).catch((err) => { // Server-side error message
+        // Remove the ember-data record from the Store
+        this.store.unloadRecord(room);
+        // Pass any error messages (i.e., server-side validation) into the UI
+        this.set('currentModel.newRoom.errors', (err.errors || []).mapBy('detail'));
+        // Notification of failure!
+        this.get('flashMessages').danger(`Problem creating room: ${data.name}`);
+      });
+    },
+
+    /* Delete a room */
+    removeRoom(room) {
+      if (window.confirm('Are you sure?')) { // Old school confirmation prompt
+        room.destroyRecord().then(() => { // Successful destruction
+          this.get('flashMessages').success(`Deleted room: ${room.get('name')}`);
+        }).catch(() => { // Unsuccessful destruction
+          this.get('flashMessages').danger(`Problem deleting room: ${room.get('name')}`);
+        });
+      }
+    }
+  },
+
+  model() {
+    return RSVP.hash({
+      // Live array of all rooms
+      rooms: this.store.findAll('room'),
+      // Object to use for creating a new room
+      newRoom: {name: '', errors: []}
+    });
+  }
 });
